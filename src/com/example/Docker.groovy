24e6e5287ca0142b7b9017e0f00cf53eeebcac4b
york zhang <yorkzhangsir@gmail.com>
2026-01-15 14:46:54 +0800

    fix-bug

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -7,13 +7,13 @@ class Docker implements Serializable {
         this.script = script
     }
 
-    def buildDockerImg(String ImageName) {
+    def buildDockerImg(String imageName) {
         script.echo 'building the docker image...'
         script.withCredentials([script.usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
             // sh 'docker build -t 0yorkzhang0/demo-app:jma-2.0 .'
-            script.sh "docker build -t ${script.imageName} ."
+            script.sh "docker build -t $imageName ."
             script.sh "echo '${script.PASS}' | docker login -u '${script.USER} --password-stdin'"
-            script.sh "docker push ${script.imageName}"
+            script.sh "docker push $imageName"
         }
     }
 }
